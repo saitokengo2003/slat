1d957789944dd88ae18371d4678595c59faff767
WakameOnigiri <20224015-honjoryoma@hcs.ac.jp>
2025-11-07 14:55:30 +0900

    グループアイコン変更、検索欄変更、グループ作成画面変更

--- src/main/java/com/sysdev/slat/controller/AccountController.java
+++ src/main/java/com/sysdev/slat/controller/AccountController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AccountController {
   @GetMapping("/accountadmin")
   public String getAccountadmin() {
+
     return "accountadmin/index";
   }
 
