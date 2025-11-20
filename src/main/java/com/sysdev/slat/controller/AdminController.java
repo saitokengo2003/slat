f09ea71c4007cae03037e1d28e2e6fac464e1d38
斉藤賢悟 <20224009-saitokengo@hcs.ac.jp>
2025-11-20 09:42:50 +0900

    fix: ログイン中ユーザ名表示の修正

--- src/main/java/com/sysdev/slat/controller/AdminController.java
+++ src/main/java/com/sysdev/slat/controller/AdminController.java
@@ -1,13 +1,25 @@
 package com.sysdev.slat.controller;
 
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.sysdev.slat.user.UserData;
 
 @Controller
 public class AdminController {
 
+  private final String SESSION_USER_DATA_KEY = "userData"; // LoginControllerとキーを合わせる
+
   @GetMapping("/admin")
-  public String getAdmin() {
+  public String getAdmin(Model model, HttpSession session) {
+    UserData userData = (UserData) session.getAttribute(SESSION_USER_DATA_KEY);
+    System.out.println("[Log]" + userData);
+    if (userData != null) {
+      model.addAttribute("displayName", userData.getDisplayName());
+    }
     return "admin/index";
   }
 }
