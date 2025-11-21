8c169e2b47da8c86076548bdb1765a9520d2170d
斉藤賢悟 <20224009-saitokengo@hcs.ac.jp>
2025-11-21 10:16:24 +0900

    feat: 権限外のボタンの非活性化

--- src/main/java/com/sysdev/slat/controller/MainController.java
+++ src/main/java/com/sysdev/slat/controller/MainController.java
@@ -30,6 +30,7 @@ public class MainController {
     if (userData != null) {
       model.addAttribute("loggedInUserId", userData.getUserId());
       model.addAttribute("displayName", userData.getDisplayName());
+      model.addAttribute("role", userData.getRoleCode());
       // 必要であればIDやロールも渡せます
       // model.addAttribute("loginUser", userData.getUserId());
       // model.addAttribute("roleCode", userData.getRoleCode());
